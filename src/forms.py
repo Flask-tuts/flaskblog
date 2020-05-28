b4f39cdb3c6237e459456e51b1d207bdb5123853
suraj@raavana <ptksuraj99@gmail.com>
2020-05-28 17:15:05 +0530

    Update Username and Email

--- src/forms.py
+++ src/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from src.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -27,3 +29,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if not current_user.username == username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Username is already taken!')
+
+    def validate_email(self, email):
+        if not current_user.email == email.data:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is already registered!')
